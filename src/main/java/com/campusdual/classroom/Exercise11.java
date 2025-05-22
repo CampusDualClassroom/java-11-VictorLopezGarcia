7e4480c574ea1d2ac9859ca14720d8a65dacfa45
VictorLopezGarcia <victor.lopezg@udc.es>
2025-05-22 10:07:02 +0200

    Ejercicio11

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,29 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+        System.out.println(redRemote.turnOn());
+        System.out.println(blackRemote.turnOn());
+        redRemote.channelUp();
+        blackRemote.channelUp();
+        redRemote.channel = 0;
+        redRemote.channelDown();
+        blackRemote.channel = 0;
+        blackRemote.channelDown();
+        System.out.println(redRemote.channel);
+        System.out.println(blackRemote.channel);
+        redRemote.volumeUp();
+        blackRemote.volumeUp();
+        redRemote.volumeDown();
+        blackRemote.volumeDown();
+        redRemote.volume = 0;
+        redRemote.volumeDown();
+        blackRemote.volume = 0;
+        blackRemote.volumeDown();
+        System.out.println(redRemote.volume);
+        System.out.println(blackRemote.volume);
+        System.out.println(redRemote.getColor());
+        System.out.println(blackRemote.getColor());
+
     }
 
 }
